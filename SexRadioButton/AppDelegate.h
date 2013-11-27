13fcafc559012a89a3a87687d81e9ac1331e3e4f
david.liu <liuchong1228@yahoo.cn>
2013-11-27 17:54:52 +0800

    Initial Commit

---
+++ SexRadioButton/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SexRadioButton
+//
+//  Created by david on 13-11-27.
+//  Copyright (c) 2013年 david. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SexRadioButton/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SexRadioButton
+//
+//  Created by david on 13-11-27.
+//  Copyright (c) 2013年 david. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
